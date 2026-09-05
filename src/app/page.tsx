2efe004c542d69ee9b6896485ee51a65f9cd74f1
youhei usami <usao9110@icloud.com>
2026-09-05 10:02:32 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,21 @@ export default function Home() {
 
         <a className={styles.brand} href="#">
           <span className={styles.logo}>◯</span>
+          <span>
+            <strong>amenowa organic</strong>
+            <small>NATURAL COSME</small>
+          </span>
+
+          <nav className={styles.actions}>
+            <a className={styles.iconButton} href="#">
+              ⌂
+            </a>
+            <button className={styles.cart}>🛒 カート 0</button>
+            <button className={styles.login}>→ ログイン</button>
+          </nav>
         </a>
       </header>
+      <main></main>
     </>
   );
 }
